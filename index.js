4ec53e0f8dee6e80ee067711a4852221cf05f9dd
Jeremie Amano <amanojeremie@gmail.com>
2018-10-13 01:10:03 -0500

    Mic pipes to Speaker

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 var record = require('node-record-lpcm16')
 const Speaker = require('speaker');
+const fs = require('fs');
 
 // Create the Speaker instance
 const speaker = new Speaker({
-  channels: 2,          // 2 channels
+  channels: 1,          // 2 channels
   bitDepth: 16,         // 16-bit samples
-  sampleRate: 44100     // 44,100 Hz sample rate
+  sampleRate: 82000 * 2.4     // 44,100 Hz sample rate
 });
 
-record.start({recordProgram : 'sox', verbose: true}).pipe(speaker)
+
+const recordOpts = {
+  recordProgram : 'sox',
+  device: 'Mic', 
+  silence: 10000,
+  sampleRate    : 41000,  // audio sample rate
+  channels      : 2,
+}
+record.start(recordOpts).pipe(speaker)
 
 setTimeout(function () {
   record.stop()
-}, 3000)
+}, 60000000)
