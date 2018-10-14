818e6ea1b440250204f5f0777bbb301112fe2af1
Tyler Acosta <tyleracosta@test-152136.dhcp.missouri.edu>
2018-10-14 09:18:06 -0500

    upgrade detection algorithm

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-// const record = require('node-record-lpcm16')
-// const Speaker = require('speaker');
 const net = require('net');
-
-// Create the Speaker instance
-// const speaker = new Speaker({
-//   channels: 1,          // 2 channels
-//   bitDepth: 16,         // 16-bit samples
-//   sampleRate: 82000 * 2.4     // 44,100 Hz sample rate
-// });
-
-
-// const recordOpts = {
-//   recordProgram: 'rec',
-//   device: 'External Microphone',
-//   silence: 10000,
-//   sampleRate: 41000,  // audio sample rate
-//   channels: 2,
-// }
-
-// let toggle = false;
-// let recordStr, c;
 let klient;
 const server = net.createServer((c) => {
   // 'connection' listener
   klient = c;
-  // c.write('Echo server\r\n');
-  // c.pipe(c);
   console.log('client connected');
   c.on('data', (data) => {
     console.log(data.toString());
@@ -33,22 +10,6 @@ const server = net.createServer((c) => {
   c.on('end', () => {
     console.log('client disconnected');
   });
-  // recordStr = record.start(recordOpts);
-  // c.write(Buffer.alloc(82000 * 2.4 * 10));
-  // recordStr.pipe(c)
-
-  // setInterval(function () {
-  //   toggle = !toggle;
-  //   console.log(toggle);
-  //   if (toggle) {
-  //     //recordStr.pause();
-  //   }
-  //   else {
-  //     c.pause();
-  //     c.write(Buffer.alloc(82000 * 2.4 * 10));
-  //     c.resume();
-  //   }
-  // }, 10000)
 });
 server.on('error', (err) => {
   throw err;
@@ -59,8 +20,7 @@ server.listen(8124, () => {
 
 // ググる
 
-const baddies = ["pineapple", "apple", "Apple"];
-
+const baddies = ["apple"];
 const record = require('node-record-lpcm16');
 
 // Imports the Google Cloud client library
@@ -69,9 +29,6 @@ const speech = require('@google-cloud/speech').v1p1beta1;
 // Creates a client
 const client = new speech.SpeechClient();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
 const encoding = 'LINEAR16';
 const sampleRateHertz = 16000;
 const languageCode = 'en-US';
@@ -83,7 +40,7 @@ const request = {
     languageCode: languageCode,
     enableWordConfidence: true,
   },
-  interimResults: false, // If you want interim results, set this to true
+  interimResults: true, // If you want interim results, set this to true
 };
 
 var triggered;
@@ -99,18 +56,17 @@ const recognizeStream = client
         ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
         : `\n\nReached transcription time limit, press Ctrl+C\n`
     );
-    data.results[0].alternatives[0].words.forEach(entry => {
-      if (baddies.includes(entry.word)) {
+    baddies.forEach((bad) => {
+      let words = data.results[0].alternatives[0].transcript.split(' ');
+      if (words[words.length - 1].indexOf(bad) != -1) {
         triggered = true;
-        // console.log("holy schnikes Batman! you said a swears: " + entry.word);
+        console.log("holy schnikes Batman! you said a swears: " + bad);
         if (klient) {
-          klient.write("holy schnikes Batman! you said a swears: " + entry.word);
+          klient.write("holy schnikes Batman! you said a swears: " + bad);
         }
       }
-    });
-    triggered ? null : console.log("good child.");
-  }
-  );
+    })
+  });
 
 // Start recording and send the microphone input to the Speech API
 record
@@ -124,5 +80,4 @@ record
   })
   .on('error', console.error)
   .pipe(recognizeStream);
-
 console.log('Listening, press Ctrl+C to stop.');
--- playback.js
+++ playback.js
@@ -1,25 +1,14 @@
-// const Speaker = require('speaker');
 const { spawn } = require('child_process');
 const net = require('net');
-
-// Create the Speaker instance
-// const speaker = new Speaker({
-//   channels: 1,          // 2 channels
-//   bitDepth: 16,         // 16-bit samples
-//   sampleRate: 82000 * 2.4     // 44,100 Hz sample rate
-// });
 
 const client = net.createConnection({ port: 8124 }, () => {
   // 'connect' listener
   console.log('connected to server!');
-  // client.write('Hello, server! Love, Client.');
 });
 
-//speaker.write(Buffer.alloc(82000 * 2.4 * 10));
-// client.pipe(speaker);
 client.on('data', (data) => {
   console.log(data.toString());
-  spawn('java', ['SimulateKeyStroke.jar']);
+  spawn('java', ['SimulateKeyStroke']);
 });
 client.on('end', () => {
   console.log('disconnected from server');
