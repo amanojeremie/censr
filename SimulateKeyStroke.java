8b400162b06ca78d3024e9c5219ff158e5f7f2e5
KitKat1412 <illinoiskath@gmail.com>
2018-10-13 21:16:10 -0500

    add .jar for keystroke simulation

--- SimulateKeyStroke.java
+++ SimulateKeyStroke.java
@@ -1,20 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
 
-public static void main (String[] args){
-    try {
-    		Robot robot = new Robot();
-    		
-            // Simulate a key press
-            robot.keyPress(KeyEvent.VK_M);
-            robot.keyRelease(KeyEvent.VK_M);
+public class SimulateKeyStroke{
 
-            robot.delay (5000);
-            robot.keyPress(KeyEvent.VK_M);
-            robot.keyRelease(KeyEvent.VK_M);
-
-    } catch (AWTException e) {
-            e.printStackTrace();
-    }
+	public static void main (String[] args){
+	    try {
+	    		Robot robot = new Robot();
+	    		
+	            // Simulate a key press
+	            robot.keyPress(KeyEvent.VK_M);
+	            robot.keyRelease(KeyEvent.VK_M);
+	
+	            robot.delay (5000);
+	            robot.keyPress(KeyEvent.VK_M);
+	            robot.keyRelease(KeyEvent.VK_M);
+	
+	    } catch (AWTException e) {
+	            e.printStackTrace();
+	    }
+	}
 }
